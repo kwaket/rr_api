9e82764ced8a255c100f19a42a0109f5e2509fcc
Kwaket 38 <kwaket@live.ru>
2020-07-05 17:29:59 +0300

    rewrite auth

--- api.py
+++ api.py
@@ -18,28 +18,23 @@ API_KEY = os.getenv('API_KEY')
 API_KEY_NAME = "access_token"
 COOKIE_DOMAIN = "localtest.me"
 
-
 api_key_query = APIKeyQuery(name=API_KEY_NAME, auto_error=False)
 api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=False)
 api_key_cookie = APIKeyCookie(name=API_KEY_NAME, auto_error=False)
 
 
-async def get_api_key(
-    api_key_query: str = Security(api_key_query),
-    api_key_header: str = Security(api_key_header),
-    api_key_cookie: str = Security(api_key_cookie),
-):
-
+async def get_api_key(api_key_query: str = Security(api_key_query),
+                      api_key_header: str = Security(api_key_header),
+                      api_key_cookie: str = Security(api_key_cookie)):
     if api_key_query == API_KEY:
         return api_key_query
-    elif api_key_header == API_KEY:
+    if api_key_header == API_KEY:
         return api_key_header
-    elif api_key_cookie == API_KEY:
+    if api_key_cookie == API_KEY:
         return api_key_cookie
-    else:
-        raise HTTPException(
-            status_code=HTTP_403_FORBIDDEN, detail="Could not validate credentials"
-        )
+    raise HTTPException(
+        status_code=HTTP_403_FORBIDDEN, detail="Could not validate credentials"
+    )
 
 
 app = FastAPI(title='API для заказа выписок')
