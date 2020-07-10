bd3d5c331232ebc82de4177cc33c331809ef16a4
Kwaket 38 <kwaket@live.ru>
2020-07-10 15:11:38 +0300

    add catching exception

--- tasks.py
+++ tasks.py
@@ -1,3 +1,5 @@
+import logging
+
 from selenium.common.exceptions import WebDriverException
 
 from spyders import EGRNStatement
@@ -5,11 +7,27 @@ from spyders import EGRNStatement
 from pprint import pprint
 
 
+import services
+
+
+def _run_task_with_exception(function, task):
+    try:
+        function(task)
+    except BaseException as exc:
+        services.update_task(task['id'], {"status": "error"})
+        logging.error('stopped by worker %s', str(exc))
+
+
+
 def execute(task):
     pprint('task added')
     pprint(task)
     spyder = EGRNStatement()
-    spyder.get_application(task)
+    _run_task_with_exception(spyder.get_application, task)
+    # try:
+    #     spyder.get_application(task)
+    # except BaseException:
+    #     update_task(task['id'], {"status": "error"})
     spyder.close()
 
 
@@ -17,5 +35,9 @@ def update(task):
     pprint('updating task')
     pprint(task)
     spyder = EGRNStatement()
-    spyder.update_application_state(task['id'])
+    _run_task_with_exception(spyder.update_application_state, task)
+    # try:
+    #     spyder.update_application_state(task['id'])
+    # except BaseException:
+    #     update_task(task['id'], {"status": "error"})
     spyder.close()
