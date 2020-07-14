960f3d8fb3c52865d9b973bfd6fb0e22b64cb52c
Kwaket 38 <kwaket@live.ru>
2020-07-15 00:31:47 +0300

    rewrite services

--- services.py
+++ services.py
@@ -15,25 +15,28 @@ def _extract_filename_without_ext(filepath):
     return os.path.splitext(filename)[0]
 
 def _gen_id():
-    last = [int(_extract_filename_without_ext(f)[0])
+    last = [int(_extract_filename_without_ext(f))
             for f in glob.glob(os.path.join(APPLICATION_DIR, '*.json'))]
     return max(last) + 1
 
 
-def _gen_filename(application_dir, application_id):
+def _gen_filename(application_dir: str, application_id: int):
     return os.path.join(application_dir, str(application_id) + '.json')
 
 
-def _save_application(application: dict) -> Application:
-    filepath = _gen_filename(APPLICATION_DIR, application['id'])
-    application['id'] = int(application['id'])
+def _save_application(application: Application) -> Application:
+    filepath = _gen_filename(APPLICATION_DIR, application.id)
+    application_dict: dict = dict(application)
+    for k, v in application_dict.items():
+        if isinstance(v, datetime):
+            application_dict[k] = v.isoformat()
     with open(filepath, 'w') as writer:
-        ujson.dump(application, writer)
-    return Application(**application)
+        ujson.dump(application_dict, writer)
+    return Application(**application_dict)
 
 
 def add_application(cadnum: str) -> Application:
-    created = datetime.now().isoformat()
+    created = datetime.utcnow().isoformat()
     application = {
         'id': _gen_id(),
         'cadnum': cadnum,
@@ -41,26 +44,26 @@ def add_application(cadnum: str) -> Application:
         'updated': created,
         'status': None
     }
-    application = _save_application(application)
-    return application
+    return _save_application(Application(**application))
 
 
-def get_application(application_id: str) -> Application:
+def get_application(application_id: int) -> Application:
     filename = _gen_filename(APPLICATION_DIR, application_id)
     return Application(**ujson.load(open(filename)))
 
 
-def update_application(updated_application: dict) -> Application:
-    application = get_application(updated_application['id'])
+def update_application(application_id: int,
+                       updated_fields: dict) -> Application:
+    application = get_application(application_id)
     application_data = dict(application)
-    application_data.update(updated_application)
-    if not updated_application.get('updated'):
-        application_data['updated'] = datetime.now().isoformat()
-    return _save_application(application_data)
+    application_data.update(updated_fields)
+    if not application.updated:
+        application_data['updated'] = datetime.utcnow().isoformat()
+    return _save_application(Application(**application_data))
 
 
-def get_application_result(application: dict):
-    res = open(os.path.join(APPLICATION_DIR, application['id'],
+def get_application_result(application: Application):
+    res = open(os.path.join(APPLICATION_DIR, str(application.id),
                             'result.html')).read()
     return res
 
