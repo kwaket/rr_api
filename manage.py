a125c856a0052062617a45cdd8343edbe1507d73
Kwaket 38 <kwaket@live.ru>
2020-07-17 20:39:55 +0300

    add manage.py

---
+++ manage.py
@@ -0,0 +1,25 @@
+import click
+
+
+@click.command()
+@click.option('--deploy',
+              is_flag=True,
+              help='Deploy database, model state',)
+@click.option('--tasker',
+              is_flag=True,
+              help='Кадастровый номер объекта',)
+def manage(deploy, tasker):
+    if tasker:
+        print('run tasker')
+
+    if deploy:
+        from db import SessionLocal
+        from services import insert_application_states
+
+        db = SessionLocal()
+        insert_application_states(db)
+        db.close()
+
+
+if __name__ == '__main__':
+    manage()
