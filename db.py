aa8f6e3a7032245e02e08394bc9bcf3f1feacf6f
Kwaket 38 <kwaket@live.ru>
2020-07-17 19:42:12 +0300

    rewrite db

--- db.py
+++ db.py
@@ -4,9 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 from settings import SQLALCHEMY_DATABASE_URL
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
