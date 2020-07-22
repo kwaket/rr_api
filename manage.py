ca3272168f5039025bbce5e2d9c9d290bb23d372
Kwaket 38 <kwaket@live.ru>
2020-07-23 01:45:53 +0300

    edit manage

--- manage.py
+++ manage.py
@@ -2,25 +2,36 @@
 import click
 
 
+@click.group()
+def cli():
+    pass
+
 @click.command()
-@click.option('--deploy',
-              is_flag=True,
-              help='Deploy database, model state',)
-@click.option('--tasker',
-              is_flag=True,
-              help='Кадастровый номер объекта',)
-def manage(deploy, tasker):
-    if tasker:
-        print('run tasker')
+def runserver():
+    click.echo('Run server at 127.0.0.1:8000')
+    import uvicorn
+    uvicorn.run("app.api:app", host="127.0.0.1", port=8000, log_level="info")
 
-    if deploy:
-        from db import SessionLocal
-        from services import insert_application_states
 
-        db = SessionLocal()
-        insert_application_states(db)
-        db.close()
+@click.command()
+def tasker():
+    click.echo('Run tasker')
+
+
+@click.command()
+def deploy():
+    from db import SessionLocal
+    from services import insert_application_states
+
+    db = SessionLocal()
+    insert_application_states(db)
+    db.close()
+
+
+cli.add_command(runserver)
+cli.add_command(tasker)
+cli.add_command(deploy)
 
 
 if __name__ == '__main__':
-    manage()
+    cli()
