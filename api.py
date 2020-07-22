c207d3275c848d81041dee068037b9b170e58c6a
Kwaket 38 <kwaket@live.ru>
2020-07-22 23:47:16 +0300

    delete useless comment

--- api.py
+++ api.py
@@ -156,12 +156,3 @@ async def add_application(application: schemas.Application,
     queue.enqueue(services.order_application, application)
     queue_low.enqueue(services.update_application_data, application)
     return application
-
-
-# @app.post("/users/", response_model=schemas.User)
-# def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-#     db_user = crud.get_user_by_email(db, email=user.email)
-#     if db_user:
-#         raise HTTPException(status_code=400, detail="Email already registered")
-#     return crud.create_user(db=db, user=user)
-
