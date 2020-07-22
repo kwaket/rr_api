7d2d59359cc77a4567a45df40f727f8e2e9f8611
Kwaket 38 <kwaket@live.ru>
2020-07-22 23:46:28 +0300

    rewrite selenium test

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -1,13 +1,33 @@
+import os
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+from settings import SAVED_RESPONSES
 
-class TestLogin():
+class TestSelenium():
 
     @pytest.fixture
     def driver(self, request):
-        driver_ = webdriver.Chrome()
+        options = webdriver.ChromeOptions()
+        prefs = {
+            "download.default_directory": SAVED_RESPONSES,
+            "download.prompt_for_download": False,
+            "download.directory_upgrade": True
+        }
+        options.add_experimental_option('prefs', prefs)
+        if os.getenv('RR_APPLICATIONS_API_CONFIG') == 'DEV':
+            driver_ = webdriver.Chrome(options=options)
+        else:
+            options.add_argument("--headless")
+            options.add_argument("--no-sandbox")
+            options.add_argument("--disable-gpu")
+            options.add_argument("--remote-debugin-port=9222")
+            options.add_argument("--screen-size=1200x800")
+            driver_ = webdriver.Remote(
+                command_executor='http://localhost:4444/wd/hub',
+                desired_capabilities=options.to_capabilities())
 
         def quit():
             driver_.quit()
