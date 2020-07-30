73e470b143c49a2248570c331c03b70ec180353f
Savva <kwaket@outlook.com>
2020-07-30 15:02:27 +0300

    fix migrations env

--- migrations/env.py
+++ migrations/env.py
@@ -8,8 +8,8 @@ from alembic import context
 
 sys.path = ['', '..'] + sys.path[1:]
 
-from db import Base, SQLALCHEMY_DATABASE_URL
-from models import *
+from app.db import Base, SQLALCHEMY_DATABASE_URL
+from app.models import *
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
